306df461b3065ac540a34f07712b390681308c5f
allagoniesareinthepast <6182018n@naver.com>
2019-10-07 20:26:38 +0900

    해결

--- string_fromat.py
+++ string_fromat.py
@@ -5,8 +5,9 @@ class Bic:
         self.pen_type = pen_type
 
     def disp(self):
-        return "Pen Info : %s" % (self.name, self.color, self.pen_type)
+        # format해야할 attribute는 3개인데, %s를 하나만 적어줘서... 갯수를 맞추고 해결함.
+        return "Pen Info : %s, %s, %s" % (self.name, self.color, self.pen_type)
 
 
 p2 = Bic("sign pen", "black", "ball")
-print(p2.disp()) # TypeError: not all arguments converted during string formatting
+print(p2.disp())  # TypeError: not all arguments converted during string formatting
